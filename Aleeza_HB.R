4d3b75fe3b5c5ea8f91040304384d70676fab577
JLSJG <85304049+JLSJG@users.noreply.github.com>
2023-10-10 22:48:31 -0500

    Add files via upload

---
+++ Aleeza_HB.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(ggplot2)
+library(readr)
+
+HB_Aleeza <- readxl::read_xlsx("/Users/javiersanjuan/Desktop/Aleeza_name.xlsx")
+  
+ggplot(HB_Aleeza, aes(x = x, y = y, color = letter)) + 
+  geom_point(size = 3) + 
+  theme_linedraw() + 
+  labs(x = "FROM MICROSTATS", 
+       y = "") + 
+  theme(legend.position = "none", 
+        axis.title.x = element_text(face = "bold"), 
+        axis.title.y = element_text(face = "bold"))
+
+
